ed6daa4ae9268554296c6406fb3faedf5c5f4370
Naoyuki Maeda <nao0777028@gmail.com>
2018-11-24 19:38:21 +0900

    クロージャ使わないパターンとPostのみ受け付けるパターンを追加

--- template.go
+++ template.go
@@ -9,5 +9,22 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
 		fmt.Fprint(w, "Hello World")
 	})
+	http.HandleFunc("/hoge", hogeHandler)
+	http.HandleFunc("/post", postHandler)
+
 	http.ListenAndServe(":8080", nil)
+}
+
+func hogeHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w, "hogehoge")
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request){
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)	// 405
+		w.Write([]byte("Only post"))
+		return
+	}
+
+	w.Write([]byte("OK"))
 }
